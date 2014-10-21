a189bf0d47c7a9a3be4d58eacdf5e96f5554257d
Balwant K <balwant.kane@xipcraft.com>
2014-10-22 00:26:48 +0530

    Simple example to use the config file

--- examples/exampleUsingConfig.php
+++ examples/exampleUsingConfig.php
@@ -1,53 +1,24 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: balwantkane
+ * User: balwantk
  * Date: 08/10/14
  * Time: 3:30 PM
  */
 
-$options = getopt("", array("config::"));
-
-set_include_path(dirname(dirname(__FILE__)));
-
-require_once 'src/whatsprot.class.php';
-require_once 'vendor/autoload.php';
-
-use Symfony\Component\Yaml\Parser;
-
 set_time_limit(10);
+require_once 'src/whatsprot.class.php';
 
-$yaml   = new Parser();
-$config = $yaml->parse(file_get_contents($options["config"]));
+// Get config path as a command line option
+$options = getopt("", array("config::"));
+$target   = "**contact's phone number**";   // Destination telephone number including the country code without '+' or '00'.
 
-########## DO NOT COMMIT THIS FILE WITH YOUR CREDENTIALS ###########
-///////////////////////CONFIGURATION///////////////////////
-//////////////////////////////////////////////////////////
-$username = $config["phone_number"];    // Telephone number including the country code without '+' or '00'.
-$password = $config["wh_password"];     // A server generated Password you received from WhatsApp. This can NOT be manually created
-$identity = $config["wh_identity"];     // Obtained during registration with this API or using MissVenom (https://github.com/shirioko/MissVenom) to sniff from your phone.
-$nickname = $config["wh_nickname"];     // This is the username (or nickname) displayed by WhatsApp clients.
-$target   = $config["test_target"];     // Destination telephone number including the country code without '+' or '00'.
-$debug    = true;                       // Set this to true, to see debug mode.
-///////////////////////////////////////////////////////////
-
-
-//Change to your time zone fromthe config file
-if (isset($config["app_time_zone"])) {
-    date_default_timezone_set($config["app_time_zone"]);
-}
-
-echo print_r($config);
-
-// php examples/exampleUsingConfig.php --config='examples/sample_config.yaml'
-// php examples/exampleUsingConfig.php --config='configs/test_config.yaml'
-
-echo "[] Logging in as '$nickname' ($username)\n";
-//Create the whatsapp object and setup a connection.
-$w = new WhatsProt($username, $identity, $nickname, $debug, $config);
+//Create the whatsapp object and setup a connection using the config file.
+$w = WhatsProt::constructWithConfig($options['config']);
 $w->connect();
 
-// Now loginWithPassword function sends Nickname and (Available) Presence
-$w->loginWithPassword($password);
-$w->sendMessage($target, "Guess the number :)");
+// Now loginWithConfiguredPassword function sends Nickname and (Available) Presence
+$w->loginWithConfiguredPassword();
+$w->sendMessage($target, "Config file works!");
 
+// php examples/exampleUsingConfig.php --config='examples/sample_config.yaml'
